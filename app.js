dd92dae52305236921338454a5004614fe9c864a
kirstenpoulsgaard <115927729+kirstenpoulsgaard@users.noreply.github.com>
2022-11-12 12:37:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+let currenturl=new URL(window.location.href);
+const txtcontent=document.querySelector(".txtcontent"); 
+
+switch(currenturl.pathname) {
+    case "/":
+        
+        fetch("/tekster/index.txt")
+        .then(
+            res=>res.text()
+        )
+        .then(    
+            data=>{  
+                txtcontent.innerHTML=data.replace(/(\r\n|\r|\n)/g, '<br>');;
+            }
+        );
+      break;
+    case "/index.html":
+        
+        fetch("/tekster/index.txt")
+        .then(
+            res=>res.text()
+        )
+        .then(    
+            data=>{  
+                txtcontent.innerHTML=data.replace(/(\r\n|\r|\n)/g, '<br>');;
+            }
+        );
+      break;
+    case "/udstillinger.html":        
+        fetch("/tekster/udstillinger.txt")
+        .then(
+            res=>res.text()
+        )
+        .then(    
+            data=>{  
+                txtcontent.innerHTML=data.replace(/(\r\n|\r|\n)/g, '<br>');;
+            }
+        );
+    break;
+    case "/kontakt.html":        
+        fetch("/tekster/kontakt.txt")
+        .then(
+            res=>res.text()
+        )
+        .then(    
+            data=>{  
+                txtcontent.innerHTML=data.replace(/(\r\n|\r|\n)/g, '<br>');;
+            }
+        );
+    break;
+    case "/profil.html":        
+        fetch("/tekster/profil.txt")
+        .then(
+            res=>res.text()
+        )
+        .then(    
+            data=>{  
+                txtcontent.innerHTML=data.replace(/(\r\n|\r|\n)/g, '<br>');;
+            }
+        );
+    break;
+    default:
+        //console.log(curentpathname);
+      
+  }
